4c4b33829c2ce9a9354081a380c0035d7353de0d
abhi-vk <abhishekabhi4123@gmail.com>
2025-01-03 21:26:21 +0530

    cors error resolved

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,10 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
